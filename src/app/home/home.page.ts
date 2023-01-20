b677ce6093182832098057e6497727ce76096d22
David Teixeira Pereira Ferreira <davidbenavraham.ti@gmail.com>
2023-01-20 01:36:19 -0300

    adicionando  novas alterções esquecidas

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,34 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  precoAlcool: string = ""
+  precoGasolina: string = ""
+
+  public resultado: String = "Resultado"
+  
   constructor() {}
+
+  calcular(){
+
+    //validar se os campos foram preenchidos
+    if(this.precoAlcool && this.precoGasolina){
+      
+      var pAlcool   = parseFloat(this.precoAlcool)
+      var pGasolina = parseFloat(this.precoGasolina)
+
+      var res = pAlcool / pGasolina
+      if(res >= 0.7){
+        this.resultado = "Melhor utilizar Gasolina"
+      }
+      else{
+        this.resultado = "Melhor utilizar o Álcool"
+      }
+
+    }
+    else{
+      this.resultado = "Preencha corretamente os campos"
+    }
+
+  }
 
 }
